b0f36a01c100c0ba1de9a0d3b25849ac081cf285
gaurav0973 <codejourneypanda@gmail.com>
2025-11-20 14:42:46 +0530

    error

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-const UserSchema = new Schema(
+const UserSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
     name: { type: String, required: true },
