99932547baa916f7d037a061dd62640d2249c408
gaurav0973 <codejourneypanda@gmail.com>
2025-11-20 14:26:41 +0530

    add inngest for syncing

---
+++ app/api/inngest/route.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    // import your inngest functions here
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion,
+  ],
+});
---
+++ models/User.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from "mongoose"
+import { unique } from "next/dist/build/utils"
+
+
+const UserSchema = new Schema({
+    _id : { type: String, required: true },
+    name : { type: String, required: true },
+    email : { type: String, required: true, unique  : true },
+    imageUrl : { type: String },
+    cartItems : { type: Object, default: {} },
+
+}, {minimize : true})
+
+const User = mongoose.model.user || mongoose.model("user", UserSchema)
+export default User
