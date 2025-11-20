5cda1103f941c1d75d903c3c4b7fb4294531a49a
gaurav0973 <codejourneypanda@gmail.com>
2025-11-20 17:39:34 +0530

    Ddb fix

---
+++ app/api/user/data/route.js
@@ -0,0 +1,23 @@
+import connectDB from "@/config/db";
+import User from "@/models/User";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(req){
+    try {
+        const {userId} = getAuth(req)
+        console.log(req)
+        
+        await connectDB()
+        const user = await User.findById(userId)
+        if(!user){
+            return NextResponse.json({success : false, message : "User not found"})
+        }
+
+        return NextResponse.json({success : true,  user})
+
+
+    } catch (error) {
+        return NextResponse.json({success : false, message : error.message})
+    }
+}
