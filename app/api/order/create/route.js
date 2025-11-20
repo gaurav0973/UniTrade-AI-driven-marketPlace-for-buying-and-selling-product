f2d1c17f4c3d1eaeba114b2a4080f1218bf9af8b
gaurav0973 <codejourneypanda@gmail.com>
2025-11-21 00:26:33 +0530

    creating placong ordr functionality

---
+++ app/api/order/create/route.js
@@ -0,0 +1,45 @@
+import Product from "@/models/Product";
+import { auth } from "@clerk/nextjs/server";
+import { accessedDynamicData } from "next/dist/server/app-render/dynamic-rendering";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request) {
+    try {
+
+        const {userId} = await auth()
+        const { address, items } = await request.json();
+
+        if(!address || !items || items.length === 0){
+            return NextResponse.json({success : false, message : "Invalid Data"})
+        }
+
+        // calculate total amount
+        const amount = items.reduce(async(acc, item) => {
+            const product = await Product.findById(item.productId);
+            return acc + (product.offerPrice * item.quantity);
+        }, 0)
+
+        await inngest.send({
+            name : "order/created",
+            data : {
+                userId,
+                address,
+                items,
+                amount : amount + Math.floor(amount*0.2),
+                date : Date.now()
+            }
+
+        })
+
+
+        // clear user cart 
+        const user = await User.findById(userId)
+        user.cartItems = {}
+        await user.save()
+
+        return NextResponse.json({success : true, message : "Order Placed successfully"})
+    } catch (error) {
+        return NextResponse.json({success : false, message : error.message})
+    }
+}
